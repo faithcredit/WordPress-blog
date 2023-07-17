eec6900d06d25f0deb3fbed6489711ee87283e6b
faithcredit <honest.dedicate@gmail.com>
2023-07-17 18:15:19 +0900

    Added Elementor plugin

---
+++ wp-content/plugins/astra-sites/inc/lib/onboarding/assets/dist/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'f191a163465dffa94587');
